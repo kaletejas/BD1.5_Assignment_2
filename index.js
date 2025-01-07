6ca09a527f45b747cfc04bb6f765db7321a07dae
kaletejas <tejasvijaykale@gmail.com>
2025-01-07 19:52:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('static'));
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
